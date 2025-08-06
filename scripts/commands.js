9b3883763fc00108ad670dcf1a3adadde7182455
Murilo Gritti <dev.murilogritti@gmail.com>
2025-08-06 20:06:15 +0000

    Ao rodar o npm obtive o seguinte resultado:
     npm run discord:register

---
+++ scripts/commands.js
@@ -0,0 +1,13 @@
+// Simple test command
+export const PING_COMMAND = {
+  name: 'ping',
+  description: 'pong',
+  // chat command (see https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types)
+  type: 1,
+};
+
+export const HELLO_COMMAND = {
+    name: 'hello',
+    description: 'Says hello to you',
+    type: 1,
+};
--- scripts/register-commands.mjs
+++ scripts/register-commands.mjs
@@ -1,39 +1,23 @@
 import 'dotenv/config';
+import { PING_COMMAND, HELLO_COMMAND } from './commands.js';
 import { DiscordRequest } from './utils.js';
 
-// Simple test command
-const TEST_COMMAND = {
-  name: 'ping',
-  description: 'Replies with pong!',
-  type: 1,
-};
+async function registerCommands() {
+  if (!process.env.DISCORD_APPLICATION_ID) {
+    console.error('DISCORD_APPLICATION_ID is not set in your .env file.');
+    process.exit(1);
+  }
 
-const HELLO_COMMAND = {
-  name: 'hello',
-  description: 'Replies with a friendly greeting!',
-  type: 1,
-};
-
-const ALL_COMMANDS = [TEST_COMMAND, HELLO_COMMAND];
-
-async function installGlobalCommands(appId, commands) {
-  // API endpoint to overwrite global commands
-  const endpoint = `applications/${appId}/commands`;
-
+  const allCommands = [PING_COMMAND, HELLO_COMMAND];
   try {
-    // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
-    const res = await DiscordRequest(endpoint, { method: 'PUT', body: commands });
+    const endpoint = `applications/${process.env.DISCORD_APPLICATION_ID}/commands`;
+    // This is a global command. It may take up to 1 hour to show up in the server.
+    const res = await DiscordRequest(endpoint, { method: 'PUT', body: allCommands });
     const data = await res.json();
-    console.log('Installed global commands:');
-    console.log(JSON.stringify(data, null, 2));
+    console.log('Successfully registered all commands:', data);
   } catch (err) {
-    console.error('Error installing global commands:', err);
+    console.error('Error registering commands:', err);
   }
 }
 
-if (!process.env.DISCORD_APPLICATION_ID) {
-    console.error('DISCORD_APPLICATION_ID is not set in your .env file.');
-    process.exit(1);
-}
-
-installGlobalCommands(process.env.DISCORD_APPLICATION_ID, ALL_COMMANDS);
+registerCommands();
